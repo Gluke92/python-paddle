0e95da3e6d954965b91736817c577c5f5ed63764
George Luke <george.luke92@gmail.com>
2020-01-20 12:18:50 -0600

    added right paddle

--- app.py
+++ app.py
@@ -6,6 +6,28 @@ wn.bgcolor("blue")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+#PaddleA
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+#PaddleB
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
 #Main game loop
 while True:
     wn.update()
+
+#Paddle A  
+
+
